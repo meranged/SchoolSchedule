46ca3d667e0eaf76ec0bf2f89c25d1cecf099a90
meranged <meranged@gmail.com>
2020-01-28 02:00:09 +0400

    What's now Fragment + ViewModel partly prepared

--- app/src/main/java/com/meranged/schoolschedule/ui/whatsnow/WhatsNowFragment.kt
+++ app/src/main/java/com/meranged/schoolschedule/ui/whatsnow/WhatsNowFragment.kt
@@ -7,13 +7,17 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.meranged.schoolschedule.database.SchoolScheduleDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import com.meranged.schoolschedule.R
+import com.meranged.schoolschedule.database.TimeSlotWithSubjects
 import com.meranged.schoolschedule.databinding.WhatsNowFragmentBinding
+import java.util.*
+import java.util.Calendar.*
 
 
 class WhatsNowFragment : Fragment() {
@@ -23,7 +27,7 @@ class WhatsNowFragment : Fragment() {
     }
 
     private lateinit var whatsNowViewModel: WhatsNowViewModel
-    lateinit var l_imageView: ImageView
+    private lateinit var whatsNowBinding:WhatsNowFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,17 +51,241 @@ class WhatsNowFragment : Fragment() {
 
 
         binding.setLifecycleOwner(this)
+        whatsNowBinding = binding
 
         var viewModelJob = Job()
 
         val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+        whatsNowViewModel.time_slots_with_subjects.observe(this, Observer {
+            it?.let {
+
+                var l_list: MutableList<TimeSlotWithSubjects> = ArrayList()
+
+                for (item in whatsNowViewModel.lessons_list){
+                    if (item.subjects.isNotEmpty()){
+                        l_list.add(item)
+                    }
+                }
+
+                whatsNowViewModel.lessons_list = l_list
+            }
+        })
+
+        whatsNowViewModel.teachers_list.observe(this, Observer {
+            it?.let {
+                whatsNowViewModel.t_list = it
+            }
+        })
+
+        whatsNowViewModel.needToChangeState.observe(this, Observer {
+            it?.let {
+                calculateNextState()
+            }
+        })
+
+        whatsNowViewModel.now_time.observe(this, Observer {
+            it?.let {
+                refreshScreen()
+            }
+        })
 
         // To use the View Model with data binding, you have to explicitly
         // give the binding object a reference to it.
         binding.whatsNowViewModel = whatsNowViewModel
 
         return binding.root
+    }
+
+    private fun calculateNextState(){
+        val calendar = Calendar.getInstance()
+        val curHour = calendar.get(HOUR_OF_DAY)
+        val curMin = calendar.get(MINUTE)
+        val weekDay = calendar.get(DAY_OF_WEEK)
+
+        val l_list = whatsNowViewModel.lessons_list
+
+
+        if (l_list.isNotEmpty()){
+
+            var firstLesson = getFirstLessonOfTheDay(weekDay)
+            var lastLesson = getLastLessonOfTheDay(weekDay)
+            if (firstLesson == null){
+            //we have a free day
+                setFreeDay()
+            } else if ((curHour < firstLesson.timeSlot.startTimeHours)
+                        or ((curHour == firstLesson.timeSlot.startTimeHours)
+                            and (curMin < firstLesson.timeSlot.startTimeMinutes))){
+            //we have a morning, before 1st lesson
+                setMorningBeforeSchool(firstLesson, calendar)
+            } else if ((curHour > firstLesson.timeSlot.startTimeHours)
+                or ((curHour == firstLesson.timeSlot.startTimeHours)
+                        and (curMin > firstLesson.timeSlot.startTimeMinutes))){
+            //we have an evening, after a last lesson
+                setEveningAfterSchool()
+            } else {
+                val curlesson = getCurrentLessonOfTheDay(weekDay, curHour, curMin)
+                if (curlesson != null){
+            //we have a current lesson
+                    if (curlesson.timeSlot.number == lastLesson!!.timeSlot.number){
+                        setLastLesson(curlesson)
+                    } else {
+                        setOrdinaryLesson(curlesson)
+                    }
+                } else {
+                    val nextLesson = getNextLesson(weekDay, curHour, curMin)
+                    setSchoolBreak(nextLesson!!)
+                }
+
+            }
+        }
+
+        for(lesson in l_list){
+
+        }
+    }
+
+    private fun getFirstLessonOfTheDay(weekDay:Int):TimeSlotWithSubjects?{
+
+        val l_list = whatsNowViewModel.lessons_list
+
+        for(lesson in l_list){
+            if (lesson.timeSlot.weekDay == weekDay)
+                return lesson
+        }
+        return null
+    }
+
+    private fun getLastLessonOfTheDay(weekDay:Int):TimeSlotWithSubjects?{
+
+        val l_list = whatsNowViewModel.lessons_list.asReversed()
+
+        for(lesson in l_list){
+            if (lesson.timeSlot.weekDay == weekDay)
+                return lesson
+        }
+        return null
+    }
+
+    //It is supposed that we have either lesson or school break
+    private fun getCurrentLessonOfTheDay(weekDay: Int, curHours: Int, curMins:Int):TimeSlotWithSubjects?{
+        val l_list = whatsNowViewModel.lessons_list
+
+        val curtimeMins = curHours*60 + curMins
+
+        for(lesson in l_list){
+            if (lesson.timeSlot.weekDay == weekDay){
+
+                val startMins = lesson.timeSlot.startTimeHours*60 + lesson.timeSlot.startTimeMinutes
+                val finishMins = lesson.timeSlot.finishTimeHours*60 + lesson.timeSlot.finishTimeMinutes
+
+                if ((curtimeMins >= startMins) and (curtimeMins < finishMins))
+                    return lesson
+            }
+        }
+        //since we haven't found a lesson than we have a break
+        return null
+    }
+
+    private fun getNextLesson(weekDay: Int, curHours: Int, curMins:Int):TimeSlotWithSubjects?{
+        val l_list = whatsNowViewModel.lessons_list
+
+
+        val curtimeMins = curHours*60 + curMins
+
+        for(lesson in l_list){
+            if (lesson.timeSlot.weekDay == weekDay){
+
+                val startMins = lesson.timeSlot.startTimeHours*60 + lesson.timeSlot.startTimeMinutes
+
+                if (curtimeMins < startMins)
+                    return lesson
+            }
+        }
+        //since we haven't found a lesson than we have a break
+        return null
+
+    }
+
+    private fun setFreeDay(){
+        whatsNowViewModel.now_status = whatsNowViewModel.FREE_TIME
+    }
+
+    private fun setMorningBeforeSchool(firstLesson: TimeSlotWithSubjects, calendar:Calendar){
+        whatsNowViewModel.now_status = whatsNowViewModel.MORNING_BEFORE_SCHOOL
+        whatsNowViewModel.currentLesson = null
+        whatsNowViewModel.nextLesson = firstLesson
+
+        val now_time_mins = calendar.get(HOUR_OF_DAY)*60 + calendar.get(MINUTE)
+        val call_time_mins = firstLesson.timeSlot.startTimeHours*60 + firstLesson.timeSlot.startTimeMinutes
+        val timerValue = (call_time_mins - now_time_mins)*60000
+
+
+        whatsNowViewModel.setTimer(timerValue.toLong())
+        refreshScreen()
+
+    }
+
+    private fun setEveningAfterSchool(){
+        whatsNowViewModel.now_status = whatsNowViewModel.EVENING_AFTER_SCHOOL
+    }
+
+    private fun setOrdinaryLesson(lesson:TimeSlotWithSubjects){
+        whatsNowViewModel.now_status = whatsNowViewModel.ORDINARY_LESSON
+    }
+
+    private fun setLastLesson(lesson:TimeSlotWithSubjects){
+        whatsNowViewModel.now_status = whatsNowViewModel.LAST_LESSON
+    }
+
+    private fun setSchoolBreak(nextLesson:TimeSlotWithSubjects){
+        whatsNowViewModel.now_status = whatsNowViewModel.SCHOOL_BREAK
+    }
+
+    private fun refreshScreen(){
+
+        whatsNowBinding.freeTimeCardView.visibility = View.GONE
+        whatsNowBinding.freeTimeCardView2.visibility = View.GONE
+        whatsNowBinding.subjectCardView.visibility = View.GONE
+        whatsNowBinding.subjectCardView2.visibility = View.GONE
+        whatsNowBinding.timeToCallCard.visibility = View.GONE
+        whatsNowBinding.timeToCallTitleCard.visibility = View.GONE
+        whatsNowBinding.whatsNextTitleCard.visibility = View.GONE
+        whatsNowBinding.whatsNowTitleCard.visibility = View.GONE
+
+
+        when (whatsNowViewModel.now_status){
+            whatsNowViewModel.UNKNOWN ->{}
+            whatsNowViewModel.MORNING_BEFORE_SCHOOL ->{
+                whatsNowBinding.whatsNowTitleCard.visibility = View.VISIBLE
+                whatsNowBinding.freeTimeCardView.visibility = View.VISIBLE
+                whatsNowBinding.freeTimeTextView.text = getStringTimeToCall(whatsNowViewModel.timeToCallCounter)
+
+            }
+            whatsNowViewModel.EVENING_AFTER_SCHOOL ->{}
+            whatsNowViewModel.FREE_TIME ->{}
+            whatsNowViewModel.ORDINARY_LESSON ->{}
+            whatsNowViewModel.LAST_LESSON ->{}
+            whatsNowViewModel.SCHOOL_BREAK -> {}
+        }
+
+    }
+
+    private fun getStringTimeToCall(timeToCall: Long):String{
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = timeToCall
+        val hour = calendar.get(HOUR_OF_DAY)
+        val mins = calendar.get(MINUTE)
+        val secs = calendar.get(SECOND)
+        var s_timeToCall:String = ""
+
+
+        if (hour > 0)
+            s_timeToCall = hour.toString() + " часов, "
+
+        s_timeToCall = s_timeToCall + mins.toString() + " минут, " + secs.toString() + " секунд"
+
+        return s_timeToCall
     }
 
 }
--- app/src/main/java/com/meranged/schoolschedule/ui/whatsnow/WhatsNowViewModel.kt
+++ app/src/main/java/com/meranged/schoolschedule/ui/whatsnow/WhatsNowViewModel.kt
@@ -1,12 +1,16 @@
 package com.meranged.schoolschedule.ui.whatsnow
 
-import android.graphics.Bitmap
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.meranged.schoolschedule.database.SchoolScheduleDao
+import com.meranged.schoolschedule.database.Subject
 import com.meranged.schoolschedule.database.Teacher
-import kotlinx.coroutines.*
-import java.io.ByteArrayOutputStream
+import com.meranged.schoolschedule.database.TimeSlotWithSubjects
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import java.sql.Time
 import java.util.*
 
@@ -22,20 +26,39 @@ class WhatsNowViewModel(
     val UNKNOWN = 0
     val ORDINARY_LESSON = 1
     val LAST_LESSON = 2
-    val BEFORE_SCHOOL = 3
-    val FREE_TIME = 4
+    val SCHOOL_BREAK = 3
+    val EVENING_AFTER_SCHOOL = 4
+    val MORNING_BEFORE_SCHOOL = 5
+    val FREE_TIME = 6
 
 
     private val viewModelJob = Job()
 
-    var calendar = Calendar.getInstance()
     val time_slots_with_subjects = db.getTimeSlotWithSubjects()
+    var lessons_list = emptyList<TimeSlotWithSubjects>()
     val teachers_list = db.getAllTeachers()
+    var t_list = emptyList<Teacher>()
     var now_status = UNKNOWN
+    var timeToCallCounter:Long = 0
+
+    var currentLesson:TimeSlotWithSubjects? = null
+    var nextLesson:TimeSlotWithSubjects? = null
+
+    private var _needToChangeState = MutableLiveData<Int>()
+
+    val needToChangeState: LiveData<Int>
+        get() = _needToChangeState
+
+    private var _now_time = MutableLiveData<Date>()
+
+    val now_time: LiveData<Date>
+        get() = _now_time
+
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
     init {
-
+        _needToChangeState.value = 0
+        _now_time.value = Calendar.getInstance().time
     }
 
     /** Coroutine setup variables */
@@ -47,5 +70,29 @@ class WhatsNowViewModel(
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
+    }
+
+    fun setTimer(timeToCall: Long){
+
+        timeToCallCounter = timeToCall
+
+        object : CountDownTimer(timeToCall, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeToCallCounter = millisUntilFinished
+                _now_time.value = Calendar.getInstance().time
+            }
+
+            override fun onFinish() {
+                _needToChangeState.value = _needToChangeState.value!! + 1
+            }
+        }.start()
+    }
+
+    fun getTeacherOfSubject(subject: Subject):Teacher?{
+        for (teacher in t_list){
+            if (subject.teacherId == teacher.teacherId)
+                return teacher
+        }
+        return null
     }
 }
